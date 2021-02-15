104a15e9d13628d4b287f6d8a20506e6b332e0c8
Joseph Vasconcelos <joseph.vasconcelos97@gmail.com>
2021-02-15 14:59:05 -0500

    Updated for new parameters

--- Demo.py
+++ Demo.py
@@ -1,13 +1,15 @@
 import Secrets
 import requests
 import json
-
+import sqlite3
+from typing import Tuple
+import random
 
 
 def get_data():
     all_data = []
     page = 0
-    for page in range(160):
+    for page in range(5):
 
         response = requests.get(f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=school.name,school.state,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall&api_key={Secrets.api_key}&page={page}")
         if response.status_code != 200:
@@ -22,14 +24,63 @@ def get_data():
 
     return all_data
 
+
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename)# connect to existing DB or create new one
+    cursor = db_connection.cursor()#get ready to read/write data
+    return db_connection, cursor
+
+
+
+
+def close_db(connection: sqlite3.Connection):
+    connection.commit()#make sure any changes get saved
+    connection.close()
+
+
+def setup_db(cursor: sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS uni_data(
+    school_name,
+    school_state,
+    TwentyEighteen_student_size,
+    TwentySeventeen_student_size,
+    TwentySeventeen_earnings,
+    TwentySixteen_repayment 
+    );''')
+
+def api_data(cursor: sqlite3.Cursor):
+    all_data = get_data()
+    for i in range(len(all_data)):
+        data1 = all_data[i]['school.name']
+        data2 = all_data[i]['school.state']
+        data3 = all_data[i]['2018.student.size']
+        data4 = all_data[i]['2017.student.size']
+        data5 = all_data[i]['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line']
+        data6 = all_data[i]['2016.repayment.3_yr_repayment.overall']
+        cursor.execute('INSERT INTO uni_data VALUES(?, ?, ?, ?, ?, ?)',
+                            (data1, data2, data3, data4, data5, data6))
+
+
+def main():
+    conn, cursor = open_db("uni_data.sqlite")
+    setup_db(cursor)
+    api_data(cursor)
+    print(type(conn))
+    close_db(conn)
+
+
+
+
+
+
 def saveToFile(all_data): #saving all data to a json file.
     with open('file.json', "w") as my_data_file:
         json.dump(all_data, my_data_file)
 
 
 
-def main():
-    demo_data = get_data()
+#def main():
+    #demo_data = get_data()
 
 if __name__ == '__main__':
     main()
--- universitydata.py
+++ universitydata.py
@@ -2,10 +2,14 @@ import sqlite3
 from typing import Tuple
 import random
 
+from Demo import get_data
+
 def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
-    db_connection = sqlite3.connect(filename)#connect to existing DB or create new one
+    db_connection = sqlite3.connect(filename)# connect to existing DB or create new one
     cursor = db_connection.cursor()#get ready to read/write data
     return db_connection, cursor
+
+
 
 
 def close_db(connection: sqlite3.Connection):
@@ -19,7 +23,7 @@ def setup_db(cursor: sqlite3.Cursor):
     school_city,
     TwentyEighteen_student_size,
     TwentySeventeen_student_size,
-    TwentySeventeen_earnings ,
+    TwentySeventeen_earnings,
     TwentySixteen_repayment 
     );''')
 
